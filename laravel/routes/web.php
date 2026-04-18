f797215c59339c8f5fd3ab2b6a643aca16d7540a
stpad <sutanpangeranaryadewa@gmail.com>
2026-04-18 19:49:54 +0700

    adding field role on users migrations

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+Route::get('/cek-db', function () {
+    return DB::connection()->getDatabaseName();
+});
 
 Route::get('/', function () {
     return view('welcome');
