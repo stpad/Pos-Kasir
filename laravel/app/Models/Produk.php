b89f8b3a2cce604d318390ca3d27f658dc5c2abd
stpad <sutanpangeranaryadewa@gmail.com>
2026-04-21 08:50:50 +0700

    repaired error on "produk" model

--- laravel/app/Models/Produk.php
+++ laravel/app/Models/Produk.php
@@ -7,26 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produk extends Model
 {
-    use HasFactory;
-
-    protected $fillable = [
-        'nama',
-        'kategori_id',
-    ];
-
-    public function kategori()
-    {
-        return $this->belongsTo(Kategori::class);
-    }
-}
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Produk extends Model
-{
     protected $fillable = ['nama', 'harga_beli', 'harga', 'stok', 'stok_awal', 'deskripsi', 'kategori_id'];
 
     public function kategori()
@@ -34,3 +14,5 @@ class Produk extends Model
         return $this->belongsTo(Kategori::class);
     }
 }
+
+?>
